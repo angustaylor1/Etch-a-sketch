a3778f701a4cec00b2170e1b6f7e0acba6d88f5a
Angus Taylor <angustaylo99@gmail.com>
2025-02-14 13:26:45 +0000

    add grid to original for loop didnt work

--- java.js
+++ java.js
@@ -2,11 +2,15 @@
 
 function buildGrid() {
     const rowContainer = document.createElement("div");
+    const grid = document.querySelector("#container")
     rowContainer.setAttribute("style", "display: flex;")
     for (i = 0; i < 16; i++) {
         const rowDiv = document.createElement("div")
-        rowDiv.setAttribute("style", "width: 100px; height: 100px;")
+        rowDiv.setAttribute("style", "min-width: 50px; min-height: 50px;")
         rowContainer.appendChild(rowDiv)
-    }
-}
+        grid.appendChild(rowContainer)
+    }}
+
+
+buildGrid()
 
